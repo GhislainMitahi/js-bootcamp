29054478349fb0767ebc5f8f73f3a1f8bb4f5c83
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-08-20 10:10:23 +0200

    i'm donee with boolean

---
+++ boolean.js
@@ -0,0 +1,32 @@
+// === -equal operator
+// !== -no equal operator
+// <   -less  than operator
+// >   -greater than operator
+// <=  -less than or equal operator 
+// >=  -greater than or equal operator
+let temp = 32;
+
+// let isMyName = temp === 31;
+// let isMyName = temp !== 31;
+// let isMyName = temp < 32;
+// let isMyName = temp > 32;
+let isMyName = temp >= 32;
+
+
+// console.log(isMyName);
+
+// challenge area
+
+// create age set to your age
+// calculate is child - if they are 7 or under
+// calculate is senior - if they are 65 or older
+// Print is child value
+// print is senior value
+
+let age = 25;
+
+let _child = age <= 7;
+let _senior = age >= 65;
+
+console.log(_child);
+console.log(_senior);
