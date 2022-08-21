bced8bf2b0b96ad24140d1a6ea9f203bf37122b5
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-08-21 14:50:04 +0200

    done with scope in javascript

---
+++ scope-2.js
@@ -0,0 +1,16 @@
+//Global (name)
+// local variable
+  // local variable
+// local variable  
+let name = "Ghislain"
+
+if (true) {
+    let name = "Mitahi"
+    if (true) {
+        name  = "nicole"
+        console.log(name);
+    }
+}
+if (true) {
+    console.log(name)
+}
